e5a4c8e00fe0e90ec9da70fd939f9b76a7d3a7a5
Carlos Mena <menacarlos.asir@gmail.com>
2022-03-21 21:49:05 +0100

    Logging on debug-root level, exif stored as list, no del, no menu

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MetaPy
-Pythonic program to read and delete metadata from images.
+Simple pythonic CLI application for image metadata collection and (optionally) delete it.
+Run on Windows and Unix-based OS.
+
+# Required libreries
+* Pillow (PIL)
+
+# How to use
+Run through terminal, as it will ask for the (absolute or relative) rute file. Make sure the backlashes are doubled '\\'
+('$python metapy.py
+$ Please, provide the rute file: ')
--- metapy.py
+++ metapy.py
@@ -1,9 +1,82 @@
-# This is a python ptoject for metadata deletion on (initially) images
-## Library importation
-import os, sys
-import logging
-from PIL import Image, ExifTags
-from __all__ import *
+"""
 
-## Main script applied for provided path
-metaPy('.\\sources\\dos.jpg')
+METAPYTHON
+
+CLI python application for image metadata collection and deletion
+
+"""
+# Library importation
+# import os
+import sys
+import logging
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+"""
+ Log Setup
+ Every sdtout will be verted into the logfile (metapy.log),
+ located on the same folder where the program is run.
+ """
+
+logging.basicConfig(level=logging.INFO, filename='.\metapy.log', format='%(asctime)s %(levelname)s - %(message)s')
+
+# Image opening and tags listing
+class OpenImage:
+    def __init__(self, path):
+        self.image = Image.open(path)
+        self.exifdata = (self.image.getexif())
+
+    def image_read(self):
+        print("The file exists...")
+        exit = self.exifdata
+        img = self.image
+        items = exit.items()
+
+        # Checking exif data stored as Tags
+
+        if exit:
+            print("Has metadata:")
+            try:
+                # Use TAGS module to make EXIF data human readable
+                exif_data = {
+                    TAGS[key]: value
+                    for key, value in items
+                    if key in TAGS
+                }
+            except AttributeError:
+                pass
+            # Raise InvalidExifData
+
+            exif_data['format'] = img.format
+            print(exif_data)
+            logging.info(exif_data)
+
+        else:
+            # Printing only image info
+            print('Sorry, image has no exif data...')
+            print('Stored data: Mode - ', img.mode, ' Size - ', img.size)
+            logging('Stored data: Mode - ', img.mode, ' Size - ', img.size)
+
+
+# MetaPy
+
+def meta_py(route):
+    # Printing route 
+    print(route)
+    try:
+        # print("The file is a valid image")
+        f1 = OpenImage(route)
+        f1.image_read()
+        # img_del(img)
+    except IOError:
+        # File is not a recognizable image
+        print("The file is not a recognizable image...")
+        logging.error("The file is not a recognizable image...")
+
+    # else:
+    # print("The file does not exists. Please, check that the " \
+    # " value provided is indeed a correct path for the image...")
+
+
+# Main script applied for provided path
+meta_py('C:\\Users\\CM\\source\\repos\\MetaPy\\MetaPy\\sources\\dos.jpg')
