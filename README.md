703bf8395e5624392ca42dc4ca20ad6dc14174e0
Carlos Mena <menacarlos.asir@gmail.com>
2022-03-23 01:11:13 +0100

    Logging on different levels (not formatted), exif formatted, exec for files and directories, no del, no menu

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Run on Windows and Unix-based OS.
 Run through terminal, as it will ask for the (absolute or relative) rute file. Make sure the backlashes are doubled '\\'
 ('$python metapy.py
 $ Please, provide the rute file: ')
+
+# Extra notes
+## imghdr for image validator
+imghdr builtin module is not used as it gets deprecated on 3.11.
+Reference: https://docs.python.org/3/library/imghdr.html, https://peps.python.org/pep-0594/
--- metapy.py
+++ metapy.py
@@ -1,14 +1,25 @@
 """
 
-METAPYTHON
+METAPY
 
-CLI python application for image metadata collection and deletion
+CLI python application for image metadata collection and optional deletion
+ with two log levels (info and debug) verted into two different files.
+Not valid for images contained on byte streams.
 
+Needed libraries:
+
+- Sys
+- Logging
+- PIL (Pillow, needed of download)
+
+
+Library importation
 """
-# Library importation
-# import os
 import sys
 import logging
+import os
+from os import listdir
+from os.path import exists, isfile, join
 from PIL import Image
 from PIL.ExifTags import TAGS
 
@@ -25,30 +36,42 @@ from PIL.ExifTags import TAGS
 
  """
 
-# Creating loggers
+
+# Creating both loggers
 logger1 = logging.getLogger('user_log')
 logger2 = logging.getLogger('dev_log')
 logger1.setLevel(logging.DEBUG)
 logger2.setLevel(logging.DEBUG)
 
 # Preparing log format
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+# formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
 # Setting INFO / USER log handler to lower level
 handler1 = logging.FileHandler('.\\metapy.log')
 stream_handler1 = logging.StreamHandler()
 stream_handler1.setLevel(level=logging.INFO)
-stream_handler1.setFormatter(formatter)
+# stream_handler1.setFormatter(formatter)
 logger1.addHandler(handler1)
 
-# Setting DEBUG / DEV log handler to only debug
+# Setting DEBUG / DEV log handler to debug
 handler2 = logging.FileHandler('.\\debug-metapy.log')
 stream_handler2 = logging.StreamHandler()
 stream_handler2.setLevel(level=logging.DEBUG)
-stream_handler2.setFormatter(formatter)
+# stream_handler2.setFormatter(formatter)
 logger2.addHandler(handler2)
 
-# Image opening and tags listing
+
+"""
+OpenImage class for image-related functions.
+
+image_read() -> Image opening and metadata collection on tags,
+    listed as key-value
+
+image_copy() -> Preselected image copy on 'origin' folder
+
+image_del() -> Preselected image metadata deletion after copy
+
+"""
 
 
 class OpenImage:
@@ -57,14 +80,12 @@ class OpenImage:
         self.exifdata = (self.image.getexif())
 
     def image_read(self):
-        print("The file exists...")
         exit = self.exifdata
         img = self.image
         items = exit.items()
 
         # Checking exif data stored as Tags
         if exit:
-            print("Has metadata:")
             try:
                 # Use TAGS module to make EXIF data human readable
                 exif_data = {
@@ -75,27 +96,31 @@ class OpenImage:
             except AttributeError:
                 logger2.error('Error found on attributes')
             finally:
-                logger2.debug(exit)
-
+                logger2.debug("Raw metada: %s", exit)
             # Raise InvalidExifData
             exif_data['format'] = img.format
-            print(exif_data)
+            print("Meta data: ", exif_data,"\n")
             logger1.info("Meta data: %s", exif_data)
+            logger1.info("")
+            logger2.info("Meta data: %s", exif_data)
+            logger2.info("")
         else:
             # Printing only image info
-            no_exif = ("Sorry, image has no exif data...",
-                'Stored data: Mode - ', img.mode, ' Size - ', img.size)
-            print(no_exif)
+            no_exif = ("Sorry, image has no exif data... Stored data: Mode - ", img.mode, ' Size - ', img.size)
+            print(no_exif,"\n")
             logger1.info(no_exif)
-            logger2.info(no_exif)
+            logger1.info("")
+            logger2.debug(no_exif)
+            logger2.info("")
 
 
 # MetaPy
 
 def meta_py(route):
-    # Printing route
-    print(route)
-    logger1.info("Exec for %s", route)
+    # Printing route on log
+    print("Executing for ", route)
+    logger1.info("Executing for %s", route)
+    logger2.info("Executing for %s", route)
     try:
         # print("The file is a valid image")
         f1 = OpenImage(route)
@@ -103,14 +128,43 @@ def meta_py(route):
         # img_del(img)
     except IOError:
         # File is not a recognizable image
-        print("The file is not a recognizable image...")
-        logger1.error("The file is not a recognizable image...")
-        logger2.error("The file is not a recognizable image...")
-
-    # else:
-    # print("The file does not exists. Please, check that the " \
-    # " value provided is indeed a correct path for the image...")
+        print(route, " is not a recognizable image... \n")
+        logger1.error("%s is not a recognizable image...", route)
+        logger1.info("")
+        logger2.error("%s file is not a recognizable image...", route)
+        logger2.info("")
 
 
 # Main script applied for provided path
-meta_py('C:\\Users\\CM\\source\\repos\\MetaPy\\MetaPy\\sources\\dos.jpg')
+def main(route):
+    # Conditional structure to check if provided arg is a file or directory
+    # and consequently exec metapy
+    if (os.path.isfile(route)):
+        # If arg is a file, it will single-execute
+        print(route, "is a file and exists")
+        logger1.info("%s is a file and exists", route)
+        meta_py(route)
+    elif(os.path.exists(route)):
+        # If arg is a directory, each file is extracted
+        my_files = [f for f in listdir(route) if isfile(join(route, f))]
+        for file in my_files:
+            file = route + "\\" + file
+            meta_py(file)
+    else:
+        print(route, "is not a valid route or file \n")
+        logger1.error("%s is not a valid route or file", route)
+        logger1.info("")
+        logger2.error("%s is not a valid route or file", route)
+        logger2.info("")
+
+
+# meta_py('C:\\Users\\CM\\source\\repos\\MetaPy\\MetaPy\\sources\\dos.jpg')
+
+# negativo
+main('C:\\Users\\CM\\source\\repos\\MetaPy\\MetaPy\\sourcess')
+
+# positivo full _fichero_
+# main('C:\\Users\\CM\\source\\repos\\MetaPy\\MetaPy\\sources\\dos.jpg')
+
+# positivo directorio
+main('C:\\Users\\CM\\source\\repos\\MetaPy\\MetaPy\\sources')
